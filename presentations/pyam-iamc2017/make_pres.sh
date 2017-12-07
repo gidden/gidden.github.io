2ae318fbd8004d082be34f06147e4fc66e7d1f3e
Matthew Gidden <matthew.gidden@gmail.com>
2017-12-07 12:13:41 -0300

    ok now last try

--- presentations/pyam-iamc2017/make_pres.sh
+++ presentations/pyam-iamc2017/make_pres.sh
@@ -1,3 +1,2 @@
 jupyter nbconvert pyam-iamc2017.ipynb --to slides --reveal-prefix ../reveal.js
 mv pyam-iamc2017.slides.html index.html
-sed -i 's/\.\.\/reveal.js/https:\/\/cdnjs\.cloudflare\.com\/ajax\/libs\/reveal\.js\/3\.1\.0/g' index.html 
