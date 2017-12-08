ceb3ed5b14c74dee76f16613e1549d9e42815814
Matthew Gidden <matthew.gidden@gmail.com>
2017-12-08 11:53:27 -0300

    now correctly use submodule

--- presentations/pyam-iamc2017/make_pres.sh
+++ presentations/pyam-iamc2017/make_pres.sh
@@ -1,2 +1,2 @@
-jupyter nbconvert pyam-iamc2017.ipynb --to slides --reveal-prefix=reveal.js
+jupyter nbconvert pyam-iamc2017.ipynb --to slides --reveal-prefix=../reveal.js
 mv pyam-iamc2017.slides.html index.html
